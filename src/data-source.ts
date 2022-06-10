66ec812d2a1a41b676ccfe8570cba0076be293b0
Miguel Leite <miguelleite360@gmail.com>
2022-06-10 12:37:12 -0300

    feadt:Docker

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,16 @@
-import 'reflect-metadata'
-import { DataSource } from 'typeorm'
-import path from 'path'
-import * as dotenv from 'dotenv'
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import path from "path";
+import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
+  type: "postgres",
   username: process.env.DB_USERNAME,
   password: process.env.DB_PWD,
   database: process.env.DATABASE,
   logging: false,
-  ssl: { rejectUnauthorized: false },
-  entities: [path.join(__dirname, './entities/**/*.{js,ts}')],
-  migrations: [path.join(__dirname, './migrations/**/*.{js,ts}')],
-})
+  entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
+  migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
+});
