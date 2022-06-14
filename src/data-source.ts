905ca1c4f81a08137ec9ebe47908a558edefd08a
breno174 <breno.angelim@hotmail.com>
2022-06-14 20:50:11 -0300

    fix: refactore data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   ssl:
-    process.env.NODE_END === "production"
+    process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
       : false,
   synchronize: false,
