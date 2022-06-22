e56782b8015d94b3152c7285f505d91c326123b4
breno174 <breno.angelim@hotmail.com>
2022-06-22 14:54:44 -0300

    feat: :sparkles: add delete vacancy route

--- src/controllers/vacancy/index.ts
+++ src/controllers/vacancy/index.ts
@@ -2,10 +2,12 @@ import vacancyCreateController from "./createVacancy.controller";
 import vacancyListController from "./listVacancy.controller";
 import proprietyVacancyController from "./proprietyVacancy.controller";
 import hiredVacancyController from "./hiredVacancy.controller";
+import deleteVacancyController from "./deleteVacancy.controller";
 
 export {
   vacancyCreateController,
   vacancyListController,
   proprietyVacancyController,
   hiredVacancyController,
+  deleteVacancyController,
 };
